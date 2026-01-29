a68f19d90157b9642d478437ac95e2b09254c141
gebusstephane-bit <gebus.stephane@gmail.com>
2026-01-30 00:40:47 +0100

    Remove TypeScript ignore errors comment
    Removed comment about ignoring TypeScript errors for the build.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* On ignore les erreurs TypeScript pour le build */
   typescript: {
     ignoreBuildErrors: true,
   },
+};
+
 export default nextConfig;
+
